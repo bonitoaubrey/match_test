8856a2a39ef9f103dd2f94055941033c2b1f6b61
Bonito Aubrey <bonitoaubrey@gmail.com>
2022-09-14 12:50:11 +0300

    add fn value_in_cents

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ enum Coin {
     Quarter,
 }
 
+fn value_in_cents (coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickey => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
